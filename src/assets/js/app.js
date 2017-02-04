268af36629591235d319fa4d881b6d30de9a1a31
deniska-ba <denis.konvisar@gmail.com>
2017-02-04 16:10:02 +0200

    first load

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -37,16 +37,61 @@
 	$(document).foundation();
 
 	$(document).ready(function(){
-		//var $slider = $('.ba-slider');
+		var $slider = $('.ba-slider-works');
 
-		// $slider.slick({
-		// 	dots: true,
-		// 	infinite: false
-		// });
+		$slider.slick({
+			slide: '.ba-slider-works__slide',
+			dots: true,
+			arrows: false,
+			infinite: true,
+			slidesToShow: 1,
+
+
+		});
 	});
+
+	$(document).ready(function(){
+		var $sliderTwo = $('.ba-slider-team');
+
+		$sliderTwo.slick({
+		slide: '.ba-slider-team__slide',
+		infinite: true,
+		slidesToShow: 3,
+		slidesToScroll: 1,
+		arrows: false,
+		dots: false,
+		speed: 300,
+		// initialSlide: 4,
+		});
+	});
+
+
+
+      $(function() {
+
+        var map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: -34.397, lng: 150.644},
+          scrollwheel: false,
+          zoom: 8
+        });
+     });
+
 
 })(jQuery);
 
+	// $('.ba-container-slider').slick({
+	// 	slide: '.ba-container-slider__slide',
+	// 	prevArrow: ".ba-arrow__prev",
+	// 	nextArrow: ".ba-arrow__next",
+	// 	infinite: true,
+	// 	slidesToShow: 4,
+	// 	slidesToScroll: 1,
+	// 	dots: false,
+	// 	speed: 300,
+	// 	centerMode: true,
+	// 	initialSlide: 4,
+	// 	variableWidth: true
+	// });
 
 
 
@@ -58,9 +103,68 @@
 
 
 
+// ;(function(){
+// 	"use strict";
+
+// 	window.onload = function(){
+
+// 		var poltavaCoord = {lat: 49.592552, lng: 34.547128},
+// 		mapDiv = document.querySelector('.ba-map');
+
+// // Get poltava info before map init
+
+// var poltavaInfo = document.querySelector('.ba-poltava-info');
+
+// poltavaInfo = poltavaInfo.innerHTML;
+
+
+// var officeMap = new google.maps.Map(mapDiv, {
+// 	zoom: 17,
+// 	center: poltavaCoord,
+// 			// mapTypeId: google.maps.MapTypeId.HYBRID,
+// 			disableDefaultUI: true
+// 		});
 
 
 
+// var poltavaMarker = new google.maps.Marker({
+// 	position: poltavaCoord,
+// 	map: officeMap,
+// 	title: 'Poltava',
+// 	animation: google.maps.Animation.DROP,
+// 	icon: 'img/beetroot.svg'
+
+
+// });
+
+// 		// Set infowindow for poltava marker
+
+// 		var poltavaInfowindow = new google.maps.InfoWindow({
+// 			content: poltavaInfo
+// 		});
+
+// 		poltavaInfowindow.open(officeMap, poltavaMarker);
+
+// 		// center map on window resize
+
+// 		google.maps.event.addDomListener(window, "resize", function() {
+// 			var center = officeMap.getCenter();
+// 			google.maps.event.trigger(officeMap, "resize");
+// 			officeMap.setCenter(center);
+// 		});
+
+
+
+
+
+
+// 	};
+
+
+
+
+
+// })();
 
 
 
